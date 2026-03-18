ca90272d712c6dead83a909c7515bad34eb21e5f
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-03-18 17:19:33 +0000

    fix: update ansible configuration and inventory for clarity; modify test script to ensure proper package installation

--- scripts/test-phase1.sh
+++ scripts/test-phase1.sh
@@ -1,3 +1,3 @@
 # scripts/test-phase1.sh
-docker exec web-server dnf install --enablerepo=yum_local -y wget
+docker exec web-server dnf install --disablerepo="*" --enablerepo="yum_local" -y wget
 docker exec web-server wget --version && echo "✅ PASS" || echo "❌ FAIL"
